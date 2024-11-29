2e9d7549b6672076e3f175680363d6a253b58eb5
Daniel Cefram Ramirez <dc@rmrz.ph>
2024-11-29 13:44:44 +0800

    chore: remove files

--- functions/python.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function python --wraps=python3 --wraps=python2 --description 'alias python=python2'
-  python2 $argv; 
-end
--- functions/python2.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function python2 --wraps='~/.pyenv/versions/2.7.18/bin/python2' --description 'alias python2=~/.pyenv/versions/2.7.18/bin/python2'
-  ~/.pyenv/versions/2.7.18/bin/python2 $argv; 
-end
